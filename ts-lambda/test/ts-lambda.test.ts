d83e3d9dee2d4fac7c91846c18ab1dfefc1fb31d
Tianle Huang <tianleh@amazon.com>
2022-02-20 08:36:27 +0000

    add a unit test for Lambda in CDK
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- ts-lambda/test/ts-lambda.test.ts
+++ ts-lambda/test/ts-lambda.test.ts
@@ -1,5 +1,7 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
+// import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
 import * as TsLambda from '../lib/ts-lambda-stack';
 
 test('Empty Stack', () => {
@@ -7,7 +9,14 @@ test('Empty Stack', () => {
     // WHEN
     const stack = new TsLambda.TsLambdaStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Handler: "index.myFunction",
+      Runtime: "nodejs14.x",
+    });
+    // expectCDK(stack).to(matchTemplate({
+    //   "Resources": {}
+    // }, MatchStyle.EXACT))
 });
